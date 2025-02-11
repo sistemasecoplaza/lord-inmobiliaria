92a4d99f1be3baaa35b634ca7d7a547bd1dd8892
Khalid Samanamud <khalidsamanamud@gmail.com>
2025-02-11 18:27:54 -0500

    Update all modified files

--- src/assets/arrImageFetch.ts
+++
@@ -1,9 +0,0 @@
-import { imageFetch } from "./imageFetch";
-
-export const arrImageFetch = async (arr: any) => {
-  return await Promise.all(
-    arr.map((el: any) => {
-      return imageFetch(el);
-    })
-  );
-};
