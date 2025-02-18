8e5a47909423636fcf35e8d70475776e8f40119a
Khalid Samanamud <khalidsamanamud@gmail.com>
2025-02-18 18:05:37 -0500

    Update all modified files

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,81 +4,83 @@ import { addDotBtnsAndClickHandlers } from "./EmblaCarouselDotButton";
 import Autoplay from "embla-carousel-autoplay";
 import "../styles/global.css";
 
-const OPTIONS = { loop: true };
+document.addEventListener("astro:page-load", () => {
+  const OPTIONS = { loop: true };
 
-const emblaNode = document.querySelectorAll("#embla");
-if (!emblaNode) throw new Error("Embla node not found");
+  const emblaNode = document.querySelectorAll("#embla");
+  if (!emblaNode) throw new Error("Embla node not found");
 
-for (const el of emblaNode) {
-  const viewportNode = el.querySelector("#embla__viewport") as HTMLElement;
-  const prevBtnNode = el.querySelector("#embla__button--prev") as HTMLElement;
-  const nextBtnNode = el.querySelector("#embla__button--next") as HTMLElement;
-  const dotsNode = el.querySelector("#embla__dots") as HTMLElement;
-  const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Autoplay()]);
+  for (const el of emblaNode) {
+    const viewportNode = el.querySelector("#embla__viewport") as HTMLElement;
+    const prevBtnNode = el.querySelector("#embla__button--prev") as HTMLElement;
+    const nextBtnNode = el.querySelector("#embla__button--next") as HTMLElement;
+    const dotsNode = el.querySelector("#embla__dots") as HTMLElement;
+    const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Autoplay()]);
 
-  const onNavButtonClick = (emblaApi: any) => {
-    const autoplay = emblaApi?.plugins()?.autoplay;
-    if (!autoplay) return;
+    const onNavButtonClick = (emblaApi: any) => {
+      const autoplay = emblaApi?.plugins()?.autoplay;
+      if (!autoplay) return;
 
-    const resetOrStop =
-      autoplay.options.stopOnInteraction === false
-        ? autoplay.reset
-        : autoplay.stop;
+      const resetOrStop =
+        autoplay.options.stopOnInteraction === false
+          ? autoplay.reset
+          : autoplay.stop;
 
-    resetOrStop();
-  };
+      resetOrStop();
+    };
 
-  const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
-    emblaApi,
-    prevBtnNode,
-    nextBtnNode,
-    onNavButtonClick
-  );
-  const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
-    emblaApi,
-    dotsNode,
-    onNavButtonClick
-  );
+    const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+      emblaApi,
+      prevBtnNode,
+      nextBtnNode,
+      onNavButtonClick
+    );
+    const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+      emblaApi,
+      dotsNode,
+      onNavButtonClick
+    );
 
-  emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
-  emblaApi.on("destroy", removeDotBtnsAndClickHandlers);
-}
-const OPTIONS_START = { align: "start" as const, loop: true };
+    emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
+    emblaApi.on("destroy", removeDotBtnsAndClickHandlers);
+  }
+  const OPTIONS_START = { align: "start" as const, loop: true };
 
-const emblaNodeStart = document.querySelectorAll("#embla_start");
-if (!emblaNodeStart) throw new Error("Embla node not found");
+  const emblaNodeStart = document.querySelectorAll("#embla_start");
+  if (!emblaNodeStart) throw new Error("Embla node not found");
 
-for (const el of emblaNodeStart) {
-  const viewportNode = el.querySelector("#embla__viewport") as HTMLElement;
-  const prevBtnNode = el.querySelector("#embla__button--prev") as HTMLElement;
-  const nextBtnNode = el.querySelector("#embla__button--next") as HTMLElement;
-  const dotsNode = el.querySelector("#embla__dots") as HTMLElement;
+  for (const el of emblaNodeStart) {
+    const viewportNode = el.querySelector("#embla__viewport") as HTMLElement;
+    const prevBtnNode = el.querySelector("#embla__button--prev") as HTMLElement;
+    const nextBtnNode = el.querySelector("#embla__button--next") as HTMLElement;
+    const dotsNode = el.querySelector("#embla__dots") as HTMLElement;
 
-  const emblaApi = EmblaCarousel(viewportNode, OPTIONS_START, [Autoplay()]);
-  const onNavButtonClick = (emblaApi: any) => {
-    const autoplay = emblaApi?.plugins()?.autoplay;
-    if (!autoplay) return;
+    const emblaApi = EmblaCarousel(viewportNode, OPTIONS_START, [Autoplay()]);
+    const onNavButtonClick = (emblaApi: any) => {
+      const autoplay = emblaApi?.plugins()?.autoplay;
+      if (!autoplay) return;
 
-    const resetOrStop =
-      autoplay.options.stopOnInteraction === false
-        ? autoplay.reset
-        : autoplay.stop;
+      const resetOrStop =
+        autoplay.options.stopOnInteraction === false
+          ? autoplay.reset
+          : autoplay.stop;
 
-    resetOrStop();
-  };
+      resetOrStop();
+    };
 
-  const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
-    emblaApi,
-    prevBtnNode,
-    nextBtnNode,
-    onNavButtonClick
-  );
-  const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
-    emblaApi,
-    dotsNode,
-    onNavButtonClick
-  );
+    const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+      emblaApi,
+      prevBtnNode,
+      nextBtnNode,
+      onNavButtonClick
+    );
+    const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+      emblaApi,
+      dotsNode,
+      onNavButtonClick
+    );
 
-  emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
-  emblaApi.on("destroy", removeDotBtnsAndClickHandlers);
-}
+    emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
+    emblaApi.on("destroy", removeDotBtnsAndClickHandlers);
+  }
+});
